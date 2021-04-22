1f278d9fb9f1e2b61855a3149be59ea71662c02c
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-04-22 20:50:05 +0200

    updated solution app.js/weatherapp

--- app.js
+++ app.js
@@ -1,2 +1,77 @@
 // Go to this URL and register https://openweathermap.org/appid
 // Get your API KEY (appid)
+
+const APIKey = "62e54303a7d54e5faad6eef0a03675e2";
+const baseUrl = "https://api.weatherbit.io/v2.0/current?";
+
+// https://api.weatherbit.io/v2.0/current?lat=41.41&lon=2.19&key=62e54303a7d54e5faad6eef0a03675e2
+//const URL = `${baseUrl}lat=${latitude}&lon=${longitude}&key=${APIKey}`;
+
+const callWeatherAPIPosition = (longitude, latitude) => {
+  const URL = `${baseUrl}lat=${latitude}&lon=${longitude}&key=${APIKey}`;
+  const callFetchURL = fetch(URL)
+    //if the call goes right || 200;
+    .then((response) => response.json())
+    .then((weather_info) => {
+      console.log("First weather info", weather_info);
+      showWeatherInfo(weather_info.data[0]);
+    });
+  //if the call goes wrong || 403, 404...;
+  callFetchURL.catch((error) => console.error("Something went wrong", error));
+};
+
+const onPositionReceived = (position) => {
+  const {
+    coords: { latitude, longitude },
+  } = position;
+  console.log(latitude, longitude);
+  callWeatherAPIPosition(longitude, latitude);
+};
+
+const onPositionDenied = (error) => {
+  const { message } = error;
+  console.log(error);
+  const notificationDiv = document.getElementsByClassName("notification");
+  const notification = notificationDiv[0];
+  notification.style.display = "block";
+  const p = document.createElement("p");
+  p.innerText = message;
+  notification.appendChild(p);
+};
+
+const showWeatherInfo = (weatherObject) => {
+  console.log("Weather Info Element", weatherObject);
+
+  const {
+    city_name,
+    country_code,
+    temp,
+    weather: { description, icon },
+  } = weatherObject;
+
+  //temperature description
+  const descriptionP = document.querySelector(".temperature-description p");
+  descriptionP.innerText = description;
+
+  //location (city,country)
+  const locationP = document.querySelector(".location p");
+  locationP.innerText = `${city_name}, ${country_code}`;
+
+  //the temperature value in Celsius
+  const temperatureValueP = document.querySelector(".temperature-value p");
+  temperatureValueP.innerHTML = `${temp} °<span>C</span>`;
+
+  //we need the icon
+  const weatherIconImg = document.querySelector(".weather-icon img");
+  const iconWithoutTheFirstLetter = icon.slice(1);
+  weatherIconImg.setAttribute("src", `icons/${iconWithoutTheFirstLetter}.png`);
+};
+
+const getUserPosition = () => {
+  navigator.geolocation.getCurrentPosition(
+    (location) => onPositionReceived(location),
+    (error) => onPositionDenied(error)
+  );
+};
+
+getUserPosition();
